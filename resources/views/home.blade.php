3efd9d9650368ea90b3835cad62b3f390a457779
GianlucaPolcino <gianluca.polcino.gp@gmail.com>
2022-01-22 18:05:15 +0100

    list created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Home</title>
-</head>
-<body>
-    @include('partials.header')
-    @include('partials/jumbotron')
-    @include('partials/main')
+@extends('layouts.main')
+
+@section('content')
+    @include('partials/comicslist')
     @include('partials/section')
-    @include('partials/footer')
-</body>
-</html>
+@endsection
---
+++ resources/views/list.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container mt-5">
+        <div class="row w-100">
+            <div class="col-8">
+                <h2>
+                    Titolo
+                </h2>
+
+                <div class="w-100">
+                    <div class="gp-price d-flex justify-content-between">
+                        <span>
+                            U.S. Price:
+                        </span>
+                        <span>
+                            AVAIABLE
+                        </span>
+                    </div>
+                    <button class="btn btn-primary">Bottone</button>
+                </div>
+            </div>
+           
+        </div>
+    </div>
+    @endsection
